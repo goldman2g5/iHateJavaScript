6c78f020c2d3d2e55cb0ea3d8eab82f5e402c06d
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2022-12-15 02:35:22 +0300

    dsgfd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import CatListing from "./CatListing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Соль меф МСК</h1>
     </div>
   );
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<CatListing/>}>
+    </Route>
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App;
---
+++ src/CatListing.js
@@ -0,0 +1,22 @@
+const CatListing= () => {
+  return (
+    <div className="container">
+        <div className="card">
+            <div className="card-title">
+                <h1>Cat Listing</h1>
+            </div>
+            <div className="card-body">
+                <table className="table table-bordered">
+                    <thead className="bg-dark text-white">
+<tr>
+
+</tr>
+                    </thead>
+                </table>
+        </div>
+        </div>
+        </div>
+  );
+}
+
+export default CatListing;
